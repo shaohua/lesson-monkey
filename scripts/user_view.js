ecab4ba95f99dd9011c4008c0b7d94720a573826
Shaohua <sh.zhou@gmail.com>
2014-10-29 22:36:54 -0700

    Disable profile card for now

--- scripts/user_view.js
+++ scripts/user_view.js
@@ -17,21 +17,13 @@ var UserView = React.createClass({
     return (
       <RB.Grid className='main'>
         <RB.Row>
-          <RB.Col sm={2} className="ln-column-left">
-            <ProfileCard
-              params={this.props.params}/>
+          <RB.Col sm={3}>
+            <Folders
+              params={this.props.params}
+              data={this.props.data}/>
           </RB.Col>
-          <RB.Col sm={10} className="ln-column-right">
-            <RB.Row>
-              <RB.Col sm={3}>
-                <Folders
-                  params={this.props.params}
-                  data={this.props.data}/>
-              </RB.Col>
-              <RB.Col sm={9}>
-                <this.props.activeRouteHandler data={this.props.data}/>
-              </RB.Col>
-            </RB.Row>
+          <RB.Col sm={9}>
+            <this.props.activeRouteHandler data={this.props.data}/>
           </RB.Col>
         </RB.Row>
       </RB.Grid>

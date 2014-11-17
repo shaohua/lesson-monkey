fd6d4b62b1ba1cac099d4cfab4f810b5a97c2ffb
Shaohua <sh.zhou@gmail.com>
2014-11-16 18:47:58 -0800

    Style mailing list form

--- scripts/mailing_list.js
+++ scripts/mailing_list.js
@@ -13,12 +13,12 @@ var MailingList = React.createClass({
     return (
       <div id="mc_embed_signup">
         <form action="//howtox.us2.list-manage.com/subscribe/post?u=47d8867b2353c9e307284a618&amp;id=c3ae16e3c4" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" novalidate>
-          <input type="email" name="EMAIL" className="email" id="mce-EMAIL" placeholder="email address" required/>
+          <div>
+            <input type="email" name="EMAIL" className="email" id="mce-EMAIL" placeholder="email address" required/>
+            <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button btn"/>
+          </div>
           <div className="hidden-input">
             <input type="text" name="b_47d8867b2353c9e307284a618_c3ae16e3c4" tabindex="-1" value=""/>
-          </div>
-          <div className="clear">
-            <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button btn btn-primary"/>
           </div>
         </form>
       </div>

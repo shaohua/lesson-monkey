7b23117a8f4ef0d3bf08864139e4976915b5874a
Shaohua Zhou <sh.zhou@gmail.com>
2015-01-06 16:42:47 -0800

    WIP for replacing card pop view

--- scripts/monkey_image_text_edit.js
+++ scripts/monkey_image_text_edit.js
@@ -5,6 +5,7 @@ var _ = require('underscore'),
   Navigation = ReactRouter.Navigation,
   ContentEditable = require('./content_editable'),
   Actions = require('./actions'),
+  Rectangle = require('./utils/rectangle'),
   RB = require('react-bootstrap');
 
 var MonkeyImageTextEdit = React.createClass({
@@ -33,54 +34,50 @@ var MonkeyImageTextEdit = React.createClass({
     var card = this.props.card;
     var colorClass = this.getRandomBgColor();
 
+    var left = (<img src={card.imgUrl}/>);
+    var right = (
+      <div>
+        <h2>
+          <ContentEditable
+            isEditable={this.props.isEditable}
+            html={card.title}
+            onChange={this.handleTitleChange} />
+        </h2>
+        <div className="card-main-text">
+          <ContentEditable
+            isEditable={this.props.isEditable}
+            html={card.content}
+            onChange={this.handleContentChange} />
+        </div>
+        <div className='card-right-bottom-col'>
+          <span
+            onClick={this.onCardEdit}
+            className="icon icon-edit"></span>
+          <span>&nbsp;</span>
+          <a href={card.htmlUrl} target='_blank'>
+            <span className="icon icon-more"></span>
+          </a>
+        </div>
+      </div>
+    );
+
     return (
-      <RB.Row className='card-row'>
-        <RB.Col xs={12} className="two-squares">
-
-          <div className={"square " + colorClass}>
-            <div className="square-inner-center">
-              <img src={card.imgUrl}/>
-            </div>
-          </div>
-
-          <div className="square">
-            <div className="square-inner-left">
-              <h2>
-                <ContentEditable
-                  isEditable={this.props.isEditable}
-                  html={card.title}
-                  onChange={this.handleTitleChange} />
-              </h2>
-              <div className="card-main-text">
-                <ContentEditable
-                  isEditable={this.props.isEditable}
-                  html={card.content}
-                  onChange={this.handleContentChange} />
-              </div>
-              <div className='card-right-bottom-col'>
-                <span
-                  onClick={this.onCardEdit}
-                  className="icon icon-edit"></span>
-                <span>&nbsp;</span>
-                <a href={card.htmlUrl} target='_blank'>
-                  <span className="icon icon-more"></span>
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div
-            onClick={this.onNavPrev}
-            className="card-nav-prev">
-            <span className="icon icon-left-nav"></span>
-          </div>
-          <div
-            onClick={this.onNavNext}
-            className="card-nav-next">
-            <span className="icon icon-right-nav"></span>
-          </div>
-        </RB.Col>
-      </RB.Row>
+      <div className='card-row'>
+        <Rectangle
+          leftBgColor={colorClass}
+          left={left}
+          right={right} />
+        <div
+          onClick={this.onNavPrev}
+          className="card-nav-prev">
+          <span className="icon icon-left-nav"></span>
+        </div>
+        <div
+          onClick={this.onNavNext}
+          className="card-nav-next">
+          <span className="icon icon-right-nav"></span>
+        </div>
+      </div>
     );
   }
 });
